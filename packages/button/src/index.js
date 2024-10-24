bb66c89a3857616a8a02948ca3891f12dc56120c
lisab <lisa@lisakula.com>
2024-10-24 16:04:52 -0700

    Did a thing

--- packages/button/src/index.js
+++ packages/button/src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const App = () => <h1>This is a button</h1>;
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+export const Button = ({ children }) => {
+    return <button>{children}</button>;
+    };
